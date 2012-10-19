eda8d454164b6a103ef7bebc8bf03f9b51c3ee42
Vladimir Panteleev <vladimir@thecybershadow.net>
2012-10-19 17:59:08 +0300

    Fixes

--- pas2d.d
+++ pas2d.d
@@ -1,16 +1,24 @@
 module pas2d;
 
-import std.stdio, std.file, std.regex, std.string, std.algorithm, std.array;
+import std.algorithm;
+import std.array;
+import std.exception;
+import std.file;
+import std.path;
+import std.regex;
+import std.stdio;
+import std.string;
 
-void main()
+void main(string[] args)
 {
-	auto lines = splitLines(readText("regexes.txt"));
+	enforce(args.length==1, "Usage: pas2d < input.pas > output.d");
+	auto lines = args[0].dirName().buildPath("regexes.txt").readText().splitLines();
 	struct Pair { Regex!char from; string to; }
 	Pair[] pairs;
 	for (size_t n=0; n<lines.length; n+=3)
 		pairs ~= Pair(regex(lines[n], "g"), lines[n+1]);
 
-	foreach (line; stdin.byLine)
+	foreach (line; stdin.byLine())
 	{
 		auto spaces = countUntil!`a!=' '`(line);
 		if (spaces >= 0)
